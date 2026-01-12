d10d15e3259054e88fb6c4c728020dbf24526d27
Bpost129 <Bpost129@yahoo.com>
2026-01-12 11:56:24 -0500

    add variables for shop and user inventories, refactor table to show shop inventory

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -3,19 +3,20 @@ import InventoryList from "../../components/InventoryList/InventoryList"
 import { inventoryData } from "../../data/data"
 
 const Shop = () => {
-  const [inventory, setInventory] = useState(inventoryData)
+  const [shopInventory, setShopInventory] = useState(inventoryData)
+  const [userInventory, setUserInventory] = useState([])
 
   function handleAddItem() {
     const newItem = { _id: 62555, name: 'Magic Wand', cost: 1, weight: 2 }
     const newItemTwo = { _id: 63444, name: 'Scroll', cost: 100, weight: 5 }
-    setInventory([newItem, newItemTwo, ...inventory])
+    setShopInventory([newItem, newItemTwo, ...shopInventory])
   }
 
   return (
     <main>
       <h1>Shop</h1>
       <button onClick={handleAddItem}>Click Here</button>
-      <InventoryList inventory={inventory} />
+      <InventoryList inventory={shopInventory} />
     </main>
   )
 }
