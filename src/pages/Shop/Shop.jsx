6e637227d2f350e8b64c10e4ee4a85f6b32bd9b4
Bpost129 <Bpost129@yahoo.com>
2026-01-12 11:14:18 -0500

    add shop data, render list in shop component, style list

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,7 +1,17 @@
+import { inventoryData } from "../../data/data"
+
+
 const Shop = () => {
+  console.log('Inventory data:', inventoryData)
+
   return (
     <main>
       <h1>Shop</h1>
+      <ul>
+        {inventoryData.map(item =>
+          <li>Item: {item.name} <span>Weight: {item.weight}</span></li>
+        )}
+      </ul>
     </main>
   )
 }
