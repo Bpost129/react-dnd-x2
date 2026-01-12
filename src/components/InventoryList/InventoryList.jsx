a3bc522e30cdd07a07c6a50896098077e1c4fd91
Bpost129 <Bpost129@yahoo.com>
2026-01-12 14:51:51 -0500

    conditionally render an empty tag when no user inventory exists

--- src/components/InventoryList/InventoryList.jsx
+++ src/components/InventoryList/InventoryList.jsx
@@ -2,33 +2,38 @@ const InventoryList = ({ inventory, title, handleAddItem, handleRemoveItem }) =>
   return (
     <div className="inventory-list">
       <h2>{title}</h2>
-      <table>
-        <tr>
-          <th>Item</th>
-          <th>Cost</th>
-          <th>Weight</th>
-          <th>
-            {handleAddItem
-              ? 'Add'
-              : 'Remove'
-            }
-          </th>
-        </tr>
-        {inventory.map(item =>
+      {inventory.length
+        ?
+        <table>
           <tr>
-            <td>{item.name}</td>
-            <td>${item.cost}</td>
-            <td>{item.weight}</td>
-            <td>
-              {handleAddItem 
-                ? <button onClick={() => handleAddItem(item)}>+</button>
-                : <button onClick={() => handleRemoveItem(item)}>x</button>
+            <th>Item</th>
+            <th>Cost</th>
+            <th>Weight</th>
+            <th>
+              {handleAddItem
+                ? 'Add'
+                : 'Remove'
               }
-
-            </td>
+            </th>
           </tr>
-        )}
-      </table>
+          {inventory.map(item =>
+            <tr>
+              <td>{item.name}</td>
+              <td>${item.cost}</td>
+              <td>{item.weight}</td>
+              <td>
+                {handleAddItem 
+                  ? <button onClick={() => handleAddItem(item)}>+</button>
+                  : <button onClick={() => handleRemoveItem(item)}>x</button>
+                }
+
+              </td>
+            </tr>
+          )}
+        </table>
+        :
+        <h3 style={{fontSize: '24px'}}>Empty</h3>
+      }
     </div>
   )
 }
