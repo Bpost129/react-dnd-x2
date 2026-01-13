ba09c1d1ff4ba145cb8250a5fea7d6ffd54f4956
Bpost129 <Bpost129@yahoo.com>
2026-01-12 21:55:14 -0500

    show empty table when user inventory is empty

--- src/components/InventoryList/InventoryList.jsx
+++ src/components/InventoryList/InventoryList.jsx
@@ -2,38 +2,37 @@ const InventoryList = ({ inventory, title, handleAddItem, handleRemoveItem }) =>
   return (
     <div className="inventory-list">
       <h2>{title}</h2>
-      {inventory.length
-        ?
-        <table>
-          <tr>
-            <th>Item</th>
-            <th>Cost</th>
-            <th>Weight</th>
-            <th>
-              {handleAddItem
-                ? 'Add'
-                : 'Remove'
-              }
-            </th>
-          </tr>
-          {inventory.map(item =>
-            <tr>
-              <td>{item.name}</td>
-              <td>${item.cost}</td>
-              <td>{item.weight}</td>
-              <td>
-                {handleAddItem 
-                  ? <button onClick={() => handleAddItem(item)}>+</button>
-                  : <button onClick={() => handleRemoveItem(item)}>x</button>
-                }
-
-              </td>
-            </tr>
-          )}
-        </table>
-        :
-        <h3 style={{fontSize: '24px'}}>Empty</h3>
+      <table>
+        <tr>
+          <th>Item</th>
+          <th>Cost</th>
+          <th>Weight</th>
+          <th>
+            {handleAddItem
+              ? 'Add'
+              : 'Remove'
+            }
+          </th>
+        </tr>
+        {inventory.length
+          ? <>
+              {inventory.map(item =>
+                <tr>
+                  <td>{item.name}</td>
+                  <td>${item.cost}</td>
+                  <td>{item.weight}</td>
+                  <td>
+                    {handleAddItem 
+                      ? <button onClick={() => handleAddItem(item)}>+</button>
+                      : <button onClick={() => handleRemoveItem(item)}>x</button>
+                    }
+                  </td>
+                </tr>
+              )}
+            </>
+        : <tr>Empty</tr>
       }
+      </table>
     </div>
   )
 }
