1c32aeedc70617e16bd6d0065614c76ff3415a45
Bpost129 <Bpost129@yahoo.com>
2026-01-14 23:02:56 -0500

    add route to spell search

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar/NavBar'
 import Shop from './pages/Shop/Shop'
 import MonsterList from './pages/MonsterList/MonsterList'
 import MonsterDetails from './pages/MonsterDetails/MonsterDetails'
+import SpellSearch from './pages/SpellSearch/SpellSearch'
 import './App.css'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path='/shop' element={<Shop />} />
         <Route path='/monsters' element={<MonsterList />} />
         <Route path='/monsters/:monsterId' element={<MonsterDetails />} />
+        <Route path='spells' element={<SpellSearch />} />
       </Routes>
     </>
   )
