37f5c8b372fa07b25032c97bd7042f81c7ac1060
Bpost129 <Bpost129@yahoo.com>
2026-01-14 23:04:20 -0500

    create small search form

---
+++ src/components/SearchForm/SearchForm.jsx
@@ -0,0 +1,10 @@
+const SearchForm = () => {
+  return (
+    <form>
+      <input name="query" type="text" autoComplete="off" />
+      <button type="submit">Search</button>
+    </form>
+  )
+}
+
+export default SearchForm
