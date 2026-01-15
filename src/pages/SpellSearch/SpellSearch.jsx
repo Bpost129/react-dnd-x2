56fa88f5d36ba5bd062c450c36864688ef6ab304
Bpost129 <Bpost129@yahoo.com>
2026-01-14 23:05:40 -0500

    import search form to spell search page

--- src/pages/SpellSearch/SpellSearch.jsx
+++ src/pages/SpellSearch/SpellSearch.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react"
 
+import SearchForm from "../../components/SearchForm/SearchForm"
+
 const SpellSearch = () => {
   const [allSpells, setAllSpells] = useState([])
   
   return (
     <main className="spell-list">
       <h1>Spell Search</h1>
+      <SearchForm />
     </main>
   )
 }
